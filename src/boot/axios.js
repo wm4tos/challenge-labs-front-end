9de61f0dba1d6f5864d734f0720b4100e03e4fcd
Wesley Matos <wesley.matos@redfox.tech>
2020-01-21 16:18:56 -0300

    use interceptors to show loading

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,6 +1,31 @@
 import Vue from 'vue';
 import axios from 'axios';
+import { Loading } from 'quasar';
 
 Vue.prototype.$axios = axios.create({
   baseURL: 'https://viacep.com.br/ws',
 });
+
+axios.interceptors.request.use(
+  (req) => {
+    Loading.show();
+    return req;
+  }, (err) => {
+    Loading.hide();
+
+    Promise.reject(err);
+  },
+);
+
+axios.interceptors.response.use(
+  (r) => {
+    Loading.hide();
+
+    return r;
+  },
+  (err) => {
+    Loading.hide();
+
+    return Promise.reject(err.response.data);
+  },
+);
