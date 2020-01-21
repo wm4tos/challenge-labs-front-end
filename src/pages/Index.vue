cebb0819cc3f96851165bfb6d410681d1cbef870
Wesley Matos <wesley.matos@redfox.tech>
2020-01-21 15:12:58 -0300

    use component to show street infos

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,22 @@
 <template>
   <q-page class="flex flex-center">
+    <street-infos v-bind="street"/>
   </q-page>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import StreetInfos from 'components/StreetInfos.vue';
+
 export default {
   name: 'PageIndex',
+  components: {
+    StreetInfos,
+  },
+  computed: {
+    ...mapGetters({
+      street: 'data/getStreet',
+    }),
+  },
 };
 </script>
