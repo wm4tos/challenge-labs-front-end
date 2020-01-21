3952bb42bb74266aacb55384385a3ecdd068f30b
Wesley Matos <wesley.matos@redfox.tech>
2020-01-21 14:36:26 -0300

    made header of app

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,7 +1,29 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-
+    <q-header
+      elevated
+      class="bg-white text-black row"
+    >
+      <h1 class="col-xs-12 col-md-4">Consultar</h1>
+      <q-form
+        class="row items-center"
+        @submit="loadCep"
+      >
+        <q-input
+          label="CEP"
+          mask="#####-###"
+          v-model="cep"
+          :rules="[
+            val => val && val.length >= 9 || 'Por favor, digite um CEP válido.'
+          ]"
+        />
+        <q-btn
+          label="Buscar"
+          type="submit"
+          no-caps
+          class="self-center"
+        />
+      </q-form>
     </q-header>
 
     <q-page-container>
@@ -11,7 +33,39 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'MyLayout',
+  computed: {
+    ...mapGetters({
+      text: 'search/getCep',
+    }),
+    cep: {
+      get() {
+        return this.text;
+      },
+      set(value) {
+        this.$store.dispatch('search/SET_CEP', value);
+      },
+    },
+    formattedCep() {
+      return this.cep.replace(/-/g, '');
+    },
+  },
+  methods: {
+    async loadCep() {
+      try {
+        const { data: street } = await this.$axios.get(`${this.formattedCep}/json`);
+
+        this.$store.dispatch('data/SET_STREET', street);
+      } catch (error) {
+        this.$q.notify({
+          message: 'Não foi possível fazer a busca no momento. Por favor, tente novamente mais tarde.',
+          color: 'red-9',
+        });
+      }
+    },
+  },
 };
 </script>
