81500cb53baca79fac1baadcd5cad9950a5307d1
Wesley Matos <wesley.matos@redfox.tech>
2020-01-22 18:12:59 -0300

    write gmap component

---
+++ src/components/Gmap.vue
@@ -0,0 +1,25 @@
+<template>
+  <GmapMap
+    :center="center"
+    :zoom="17"
+    style="width: 500px; height: 300px"
+  />
+</template>
+
+<script>
+import { validateProps } from 'src/helpers';
+
+export default {
+  props: {
+    center: {
+      type: Object,
+      required: true,
+      validator(obj) {
+        const props = ['lat', 'lng'];
+
+        return validateProps(obj, ...props);
+      },
+    },
+  },
+};
+</script>
