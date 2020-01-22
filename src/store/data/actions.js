33a57e98b2e4ae8364a7ce19c132dc1b5516d1df
Wesley Matos <wesley.matos@redfox.tech>
2020-01-22 16:46:17 -0300

    write method to clean street obj

--- src/store/data/actions.js
+++ src/store/data/actions.js
@@ -1,3 +1,9 @@
+import state from './initial-state';
+
 export const SET_STREET = ({ commit }, street) => {
   commit('SAVE_STREET', street);
+};
+
+export const SET_EMPTY_STREET = ({ commit }) => {
+  commit('SAVE_STREET', state.street);
 };
