efb1adbc05f207c6d69f8be025669e338a51bb68
Wesley Matos <wesley.matos@redfox.tech>
2020-01-22 18:13:12 -0300

    use gmap component on streetdata component

--- src/components/StreetData.vue
+++ src/components/StreetData.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <street-infos v-bind="street"/>
+    <gmap :center="center"/>
   </div>
 </template>
 
 <script>
 import StreetInfos from 'components/StreetInfos';
 import { validateProps } from 'src/helpers';
+import Gmap from './Gmap';
 
 export default {
   components: {
     StreetInfos,
+    Gmap,
   },
   props: {
     street: {
@@ -28,6 +31,15 @@ export default {
           'gia',
           'unidade',
         ];
+
+        return validateProps(obj, ...props);
+      },
+    },
+    center: {
+      type: Object,
+      required: true,
+      validator(obj) {
+        const props = ['lat', 'lng'];
 
         return validateProps(obj, ...props);
       },
