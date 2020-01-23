3de9dcea66cf61886b15e317f66e3aed3102f376
Wesley Matos <wrickee@gmail.com>
2020-01-22 22:51:15 -0300

    use map-marker to open street url (on google maps)

--- src/components/Gmap.vue
+++ src/components/Gmap.vue
@@ -3,10 +3,17 @@
     :center="center"
     :zoom="17"
     style="width: 500px; height: 300px"
-  />
+  >
+    <GmapMarker
+      :position="center"
+      clickable
+      @click="redirect"
+    />
+  </GmapMap>
 </template>
 
 <script>
+import { openURL } from 'quasar';
 import { validateProps } from 'src/helpers';
 
 export default {
@@ -19,6 +26,14 @@ export default {
 
         return validateProps(obj, ...props);
       },
+    },
+  },
+  methods: {
+    redirect() {
+      const { lat, lng } = this.center;
+      const url = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+
+      openURL(url);
     },
   },
 };
