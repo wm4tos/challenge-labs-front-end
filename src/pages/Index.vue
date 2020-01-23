5c91a81b187da707ae68e1e74e80cb0e198b631d
Wesley Matos <wrickee@gmail.com>
2020-01-22 22:53:13 -0300

    show a message to all google API possible responses

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -45,7 +45,22 @@ export default {
     loadFullAddres() {
       const formatResponse = req => req.data;
       const validateIfIsError = (data) => {
-        if (data.error_message) throw new Error(data.error_message);
+        const ERROR = 'A API do Google Maps tá com um probleminha... Pode tentar de novo depois?';
+        const REQUEST_DENIED = 'Acho que... Essa página não pode fazer essa requisição. Mas ué?';
+        const OVER_QUERY_LIMIT = 'Desculpe, mas a API bateu o limite máximo de requisições :(';
+        const ZERO_RESULTS = 'Desculpe, mas essa rua não foi encontrada na API do Google Maps :/';
+        const INVALID_REQUEST = 'Isso não devia acontecer, mas acho que tem um parâmetro faltando na busca. :O';
+
+        const mapsStatus = {
+          ZERO_RESULTS,
+          OVER_QUERY_LIMIT,
+          REQUEST_DENIED,
+          ERROR,
+          UNKNOWN_ERROR: ERROR,
+          INVALID_REQUEST,
+        };
+
+        if (data.status !== 'OK') throw new Error(mapsStatus[data.status]);
 
         return data;
       };
